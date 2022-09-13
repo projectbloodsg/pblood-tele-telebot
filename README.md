92c68e525be722dfc2253ff03fb1bfbbe38d99e6
jensenhuangyankai <jensen@huangyankai.com>
2022-09-13 16:29:30 +0800

    changed message to messages

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pblood-tele-telebot
+
+Starting up the application: ```python3 main.py```
+Telebot name: pbloodTest
--- main.py
+++ main.py
@@ -3,42 +3,55 @@ from decouple import config
 import requests
 import telebot
 
-API_KEY = config('API_KEY')
+API_KEY = config('TESTING_API_KEY')
 BASE_URL = config('BASE_URL')
 
 bot = telebot.TeleBot(API_KEY)
 
 def getFirst():
-    response = requests.get("{base_url}/message/root".format(base_url=BASE_URL))
+    response = requests.get("{base_url}/messages/root".format(base_url=BASE_URL))
+    print(response)
     if response.status_code != 200:
         print("Result not found!")
 
     return response.json()
 
 def getChild(id):
-    response = requests.get("{base_url}/message/child/{childId}".format(base_url=BASE_URL, childId=id))
+    response = requests.get("{base_url}/messages/child/{childId}".format(base_url=BASE_URL, childId=id))
     if response.status_code != 200:
         print("Result not found!")
 
     return response.json()
 
 def gen_markup(id):
-    response = getChild(id)
-    markup = InlineKeyboardMarkup()
-    length = len(response)
-    markup.row_width = length
-    for i in range(length):
-        markup.add(InlineKeyboardButton(response[i]["content"], callback_data = response[i]["message_id"]))
-    return markup
+    try:
+        response = getChild(id)
+        #print(response)
+        #if response == []:
+
+        markup = InlineKeyboardMarkup()
+        length = len(response)
+        markup.row_width = length
+        for i in range(length):
+            markup.add(InlineKeyboardButton(response[i]["content"], callback_data = response[i]["message_id"]))
+        return markup
+    except Exception as e:
+        print(e)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-    response = getChild(call.data)
-    bot.send_message(call.message.chat.id, response[0]['content'], reply_markup=gen_markup(response[0]['message_id']))
+    try:
+        response = getChild(call.data)
+        bot.send_message(call.message.chat.id, response[0]['content'], reply_markup=gen_markup(response[0]['message_id']))
+    except Exception as e:
+        print(e)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    question = getFirst()
-    bot.send_message(message.chat.id, question[0]['content'], reply_markup=gen_markup(question[0]["message_id"]))
+    try:
+        question = getFirst()
+        bot.send_message(message.chat.id, question[0]['content'], reply_markup=gen_markup(question[0]["message_id"]))
+    except Exception as e:
+        print(e)
 
 bot.polling()
